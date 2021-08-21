6df7d79ede2e4a30c3261b2205d8a1d7cfeafeb7
Rajan Agarwal <rajan.ag005@gmail.com>
2021-08-21 10:46:55 -0400

    new

--- app/sagas/index.js
+++ app/sagas/index.js
@@ -71,9 +71,9 @@ export function * loginFlow () {
       logout: take(LOGOUT)
     })
     if (winner.auth) {
-      yield put({type: SET_AUTH, newAuthState: true}) // User is logged in (authorized)
-      yield put({type: CHANGE_FORM, newFormState: {username: '', password: ''}}) // Clear form
-      forwardTo('/dashboard') // Go to dashboard page
+      yield put({type: SET_AUTH, newAuthState: true}) 
+      yield put({type: CHANGE_FORM, newFormState: {username: '', password: ''}}) 
+      forwardTo('/dashboard')
     }
   }
 }
@@ -90,34 +90,26 @@ export function * logoutFlow () {
 
 export function * registerFlow () {
   while (true) {
-    // We always listen to `REGISTER_REQUEST` actions
     const request = yield take(REGISTER_REQUEST)
     const {username, password} = request.data
 
-    // We call the `authorize` task with the data, telling it that we are registering a user
-    // This returns `true` if the registering was successful, `false` if not
     const wasSuccessful = yield call(authorize, {username, password, isRegistering: true})
 
-    // If we could register a user, we send the appropiate actions
+
     if (wasSuccessful) {
-      yield put({type: SET_AUTH, newAuthState: true}) // User is logged in (authorized) after being registered
-      yield put({type: CHANGE_FORM, newFormState: {username: '', password: ''}}) // Clear form
-      forwardTo('/dashboard') // Go to dashboard page
+      yield put({type: SET_AUTH, newAuthState: true}) 
+      yield put({type: CHANGE_FORM, newFormState: {username: '', password: ''}}) 
+      forwardTo('/dashboard') 
     }
   }
 }
 
-// The root saga is what we actually send to Redux's middleware. In here we fork
-// each saga so that they are all "active" and listening.
-// Sagas are fired once at the start of an app and can be thought of as processes running
-// in the background, watching actions dispatched to the store.
 export default function * root () {
   yield fork(loginFlow)
   yield fork(logoutFlow)
   yield fork(registerFlow)
 }
 
-// Little helper function to abstract going to different pages
 function forwardTo (location) {
   browserHistory.push(location)
 }
